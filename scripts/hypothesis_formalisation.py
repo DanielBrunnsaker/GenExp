34a535d9212d9b9c1807cabc565fb8d0fb868be4
filipkro <filip.kronstrom@gmail.com>
2024-11-14 16:14:33 +0100

    example rdflib code

--- scripts/hypothesis_formalisation.py
+++ scripts/hypothesis_formalisation.py
@@ -1,8 +1,11 @@
 import rdflib
 from rdflib.namespace import RDFS, RDF, OWL
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 OBO = rdflib.Namespace('http://purl.obolibrary.org/obo/')
-
+HYPO = rdflib.Namespace('http://hypo.project-genesis.io#')
 
 def role_between_classes(a, b, r):
     bn = rdflib.BNode()
@@ -12,3 +15,16 @@ def role_between_classes(a, b, r):
     trips.append((bn, OWL.someValuesFrom, b))
     trips.append((a, RDFS.subClassOf, bn))
     return trips
+
+hypo_graph = rdflib.Graph()
+hypo_graph.parse(os.path.join(BASE_DIR, 'ontology-files/hypo.ttl'))
+
+"""
+Add A subClassOf B
+
+hypo_graph.add((HYPO.A, RDFS.subClassOf, HYPO.B))
+
+Add A <exists>rel.B
+for t in role_between_classes(HYPO.A, HYPO.B, HYPO.rel):
+    hypo_graph.add((t))
+"""
