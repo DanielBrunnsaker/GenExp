62128f7e1c9ac2e6e0ce4fab3c61e5b2c7232910
Daniel Brunnsåker <daniel.brunnsaker@gmail.com>
2025-02-18 11:33:32 +0100

    Update config.py

--- scripts/config.py
+++ scripts/config.py
@@ -6,7 +6,7 @@ PRINT_HYPOTHESIS_DURING_WORKFLOW = True
 ALLOW_FOR_CHOICE = True
 
 # Logic program prioritization settings
-REUSE_LOGIC_PROGRAM = False
+REUSE_LOGIC_PROGRAM = False # Below logic can be changed if you want to bias the experiments differently
 RELEVANCE_SCORES = {
         # predicates
         'compound_name': 5, # Reward patterns with a specific condition in mind (if there is one)
@@ -24,7 +24,7 @@ RELEVANCE_SCORES = {
 # Dispensing settings
 WELL_VOLUME = 270 # in uL
 WELL_VOLUME_UNIT = 'microliter'
-DILUTION_FACTOR = 15
+DILUTION_FACTOR = 15 # Dilution factor for preculture
 
 # Layout generation settings
 MINIZINC_PATH = "/Applications/MiniZincIDE.app/Contents/Resources/minizinc"
@@ -39,16 +39,16 @@ LLM_PATTERN_SELECTION_TEMPERATURE = 0.5
 LLM_VARIATION_TEMPERATURE = 0.5
 LLM_EXPERIMENT_SELECTION_TEMPERATURE = 0.5
 LLM_AUTOFORMALIZATION_TEMPERATURE = 0.05
-VARIATIONS = 10
+VARIATIONS = 10 # How many variations to generate from the pattern selection step
 
 # Mass spectrometry settings
 PERFORM_MASS_SPEC = True
-SAMPLES_IN_SEQUENCE = 6
-TUNE_INTERVAL = 4
+SAMPLES_IN_SEQUENCE = 6 # How many samples to include in sample blocks
+TUNE_INTERVAL = 4 # How many sample-blocks (blank, qc, sample, qc, blank) to have inbetween reference tuning injections
 RF_SETTINGS_FILE = '../data/rf_params/AutonoMS_template.xlsx'
-RUNLIST_RANDOMIZATION = True
+RUNLIST_RANDOMIZATION = True # If one wants to randomize the runlit. Recommended!
 MS_POLARITY = 'Positive'
-METHOD_NAME = '2024-10-10_AminoAcids_test_1_Pos.m'
+METHOD_NAME = '2024-10-10_AminoAcids_test_1_Pos.m' # Mass spec method
 COLUMN_TYPE = 'HILIC_241126'
 PLATE_TYPE = 'P96'
 
