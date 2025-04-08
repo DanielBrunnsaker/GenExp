757f5f41513538297b2c4c6c26eee32747a605a8
Alexander Gower <alecgower@hotmail.co.uk>
2025-04-08 20:19:10 +0000

    Changed hypothesis formalisation script to create new graph for each hypothesis

--- scripts/hypothesis_formalisation.py
+++ scripts/hypothesis_formalisation.py
@@ -44,15 +44,18 @@ def term_from_label(label, g):
     term = g.value(
         predicate=RDFS.label,
         object=rdflib.Literal(
-            label, datatype=rdflib.URIRef("http://www.w3.org/2001/XMLSchema#string")
+            label, datatype=rdflib.URIRef(
+                "http://www.w3.org/2001/XMLSchema#string")
         ),
     )
     if term is None:
-        term = g.value(predicate=RDFS.label, object=rdflib.Literal(label, lang="en"))
+        term = g.value(predicate=RDFS.label,
+                       object=rdflib.Literal(label, lang="en"))
     if term is None:
         term = g.value(predicate=RDFS.label, object=rdflib.Literal(label))
     if term is None:
-        term = g.value(predicate=OBOINOWL.hasExactSynonym, object=rdflib.Literal(label))
+        term = g.value(predicate=OBOINOWL.hasExactSynonym,
+                       object=rdflib.Literal(label))
     if term is None:
         term = g.value(
             predicate=OBOINOWL.hasRelatedSynonym, object=rdflib.Literal(label)
@@ -62,13 +65,15 @@ def term_from_label(label, g):
 
 # Set up Fuseki endpoint to create new SPARQL store
 HOST = "localhost"
-CHEBI_FUSEKI_PORT = 3030
+CHEBI_FUSEKI_PORT = 3037
 try:
-    requests.get(f"http://{HOST}:{CHEBI_FUSEKI_PORT}/genesis/query", timeout=10)
+    requests.get(
+        f"http://{HOST}:{CHEBI_FUSEKI_PORT}/genesis/query", timeout=10)
 except requests.exceptions.ConnectionError:
     # Perhaps we are in a docker container (ensure that the correct port is forwarded)
     HOST = "host.docker.internal"
-    requests.get(f"http://{HOST}:{CHEBI_FUSEKI_PORT}/genesis/query", timeout=10)
+    requests.get(
+        f"http://{HOST}:{CHEBI_FUSEKI_PORT}/genesis/query", timeout=10)
 
 chebi_query_endpoint = f"http://{HOST}:{CHEBI_FUSEKI_PORT}/genesis/query"
 chebi_store = sparqlstore.SPARQLStore(chebi_query_endpoint)
@@ -80,17 +85,21 @@ hypo_ds = rdflib.Dataset()
 hypo_ds.bind("hypo", HYPO)
 
 # Create local graph for hypothesis ontology
-hypo_graph = hypo_ds.graph(rdflib.URIRef("http://hypo.project-genesis.io"))
-hypo_graph.parse(os.path.join(BASE_DIR, "ontology-files/hypo.ttl"), format="turtle")
+hypo_onto_graph = hypo_ds.graph(
+    rdflib.URIRef("http://hypo.project-genesis.io"))
+hypo_onto_graph.parse(os.path.join(
+    BASE_DIR, "ontology-files/hypo.ttl"), format="turtle")
+hypo_base_graph = hypo_ds.graph(
+    rdflib.URIRef("http://hypo.project-genesis.io/base"))
 
 """
 Add A subClassOf B
 
-hypo_graph.add((HYPO.A, RDFS.subClassOf, HYPO.B))
+hypo_onto_graph.add((HYPO.A, RDFS.subClassOf, HYPO.B))
 
 Add A <exists>rel.B
 for t in role_between_classes(OBO.A, HYPO.B, HYPO.rel):
-    hypo_graph.add((t))
+    hypo_onto_graph.add((t))
 
 """
 
@@ -101,7 +110,8 @@ hypotheses = []
 for folder in os.listdir(hypotheses_dir):
     try:
         with open(
-            os.path.join(hypotheses_dir, folder, "hypothesis/hypothesis_details.json"),
+            os.path.join(hypotheses_dir, folder,
+                         "hypothesis/hypothesis_details.json"),
             "r",
             encoding="utf-8",
         ) as fi:
@@ -109,24 +119,30 @@ for folder in os.listdir(hypotheses_dir):
     except (FileNotFoundError, NotADirectoryError):
         continue
 
-CHEM_COMP_ACC = term_from_label("chemical compound accumulation", hypo_graph)
-CHEM_ACC_OF = term_from_label("accumulationOfChemical", hypo_graph)
-RESISTANCE_TO_CHEM = term_from_label("resistanceToChemical", hypo_graph)
-METAL_RESISTANCE = term_from_label("metalResistance", hypo_graph)
-STATE_HAS_OBSERVABLE = term_from_label("stateHasObservable", hypo_graph)
+CHEM_COMP_ACC = term_from_label(
+    "chemical compound accumulation", hypo_onto_graph)
+CHEM_ACC_OF = term_from_label("accumulationOfChemical", hypo_onto_graph)
+RESISTANCE_TO_CHEM = term_from_label("resistanceToChemical", hypo_onto_graph)
+# For now, we are using the relation that is defined from the
+# `resistance to chemicals` phenotype in APO, of which metal
+# resistance is a subclass.
+METAL_RESISTANCE = term_from_label("resistanceToChemical", hypo_onto_graph)
+STATE_HAS_OBSERVABLE = term_from_label("stateHasObservable", hypo_onto_graph)
 DEFAULT_REFERENCE_STATE = HYPO[create_id(prefix="S-REF")]
-ORGANISM_STATE = term_from_label("organismState", hypo_graph)
-hypo_graph.add((DEFAULT_REFERENCE_STATE, RDFS.subClassOf, ORGANISM_STATE))
+ORGANISM_STATE = term_from_label("organismState", hypo_onto_graph)
+hypo_base_graph.add((DEFAULT_REFERENCE_STATE, RDFS.subClassOf, ORGANISM_STATE))
 
 TEST_LOGIC_PROGRAMS = [
     "Cell(A):-exhibits_phenotype(A,'decreased metal resistance',B,C),compound_name(B,'zinc dichloride').",
     "Cell(A):-exhibits_phenotype(A,'increased resistance to chemicals',B,C),compound_name(B,fenpropimorph).",
 ]
+ATOM_FINDER = re.compile(r"([A-z0-9]+\(.+?\))(?:,|.)")
 ATOM_PARSER = re.compile(r"^(\w+)\((.+)\)$")
 
 
 def find_or_create_node(
-    graph,
+    dataset: rdflib.Dataset,
+    graph: rdflib.Graph,
     triples,
     id_prefix: Optional[str] = None,
     namespace: Optional[rdflib.Namespace] = None,
@@ -143,14 +159,16 @@ def find_or_create_node(
     """
 
     query_template = """SELECT {} WHERE {{
-        {}
+        GRAPH ?g {{
+            {}
+        }}
     }}"""
 
     query_triples = [
         tuple(map(lambda v: "?queried_node" if v is None else v, t)) for t in triples
     ]
     query = query_template.format(
-        "?queried_node",
+        "?queried_node ?g",
         "\n\t".join(
             map(
                 lambda t: " ".join(
@@ -168,7 +186,7 @@ def find_or_create_node(
             )
         ),
     )
-    qres = graph.query(query)
+    qres = dataset.query(query)
 
     if len(qres) == 0:
         # print("Need to add this node.")
@@ -184,23 +202,23 @@ def find_or_create_node(
         return next(iter(qres)).get("queried_node")
 
 
-def logic_program_to_state(logic_program, reference_state=DEFAULT_REFERENCE_STATE):
+def logic_program_to_state(logic_program, h_graph, reference_state=DEFAULT_REFERENCE_STATE):
     triples = []
     atoms = []
-    head, body = logic_program.split(":-")
-    while len(body) > 0:
-        end = body.find(")")
-        predicate, arguments = ATOM_PARSER.match(body[: end + 1]).groups()
-        atoms.append((predicate, [a.replace("'", "") for a in arguments.split(",")]))
-        if end < len(body):
-            body = body[end + 2 :]
+    head, body = logic_program.split(":-", 1)
+    for atom in ATOM_FINDER.findall(body):
+        predicate, arguments = ATOM_PARSER.match(atom).groups()
+        atoms.append((predicate, [a.replace("'", "")
+                     for a in arguments.split(",")]))
 
     def keyfunc(t):
         return t[0]
 
-    atoms = {k: list(g) for k, g in groupby(sorted(atoms, key=keyfunc), keyfunc)}
+    atoms = {k: list(g) for k, g in groupby(
+        sorted(atoms, key=keyfunc), keyfunc)}
+    # print(atoms)
     phenotype_uri_list = [
-        write_exhibits_phenotype_to_graph(args, atoms, hypo_graph)
+        write_exhibits_phenotype_to_graph(args, atoms, h_graph)
         for _, args in atoms.get("exhibits_phenotype", [])
     ]
 
@@ -212,7 +230,7 @@ def logic_program_to_state(logic_program, reference_state=DEFAULT_REFERENCE_STAT
         for t in role_between_classes(None, uri_pair[1], STATE_HAS_OBSERVABLE)
     ] + [(None, RDFS.subClassOf, ORGANISM_STATE)]
     state = find_or_create_node(
-        hypo_graph, state_triples, id_prefix="S", namespace=HYPO
+        hypo_ds, h_graph, state_triples, id_prefix="S", namespace=HYPO
     )
     # TODO: Add Label or other property to make querying easier
     return state
@@ -221,42 +239,54 @@ def logic_program_to_state(logic_program, reference_state=DEFAULT_REFERENCE_STAT
 def write_exhibits_phenotype_to_graph(
     args,
     atoms,
-    graph,
+    h_graph,
+    hypo_onto_graph=hypo_onto_graph,
     reference_state=DEFAULT_REFERENCE_STATE,
     phenotype_namespace=HYPO,
 ):
     # Get qualifier and phenotype from arguments
     qualifier, phtype_label = args[1].split(" ", maxsplit=1)
-    phtype = term_from_label(phtype_label, graph)
-    qualifying_relation = term_from_label(qualifier + "ComparedTo", graph)
+    phtype = term_from_label(phtype_label, hypo_onto_graph)
+    qualifying_relation = term_from_label(
+        qualifier + "ComparedTo", hypo_onto_graph)
+
+    # print(atoms)
+    # print(args)
 
     match phtype_label:
         case "resistance to chemicals":
-            compound_name = term_from_label(
-                next(filter(lambda t: t[1][0] == args[2], atoms.get("compound_name")))[
-                    1
-                ][1],
-                chebi,
-            )
-            additional_ref_triples = role_between_classes(
-                None, compound_name, RESISTANCE_TO_CHEM
-            )
-            additional_comp_triples = role_between_classes(
-                None, compound_name, RESISTANCE_TO_CHEM
-            )
+            try:
+                compound_name = term_from_label(
+                    next(filter(lambda t: t[1][0] == args[2], atoms.get("compound_name")))[
+                        1
+                    ][1],
+                    chebi,
+                )
+                additional_ref_triples = role_between_classes(
+                    None, compound_name, RESISTANCE_TO_CHEM
+                )
+                additional_comp_triples = role_between_classes(
+                    None, compound_name, RESISTANCE_TO_CHEM
+                )
+            except TypeError:
+                "Could not find compound name in atoms."
+
         case "metal resistance":
-            compound_name = term_from_label(
-                next(filter(lambda t: t[1][0] == args[2], atoms.get("compound_name")))[
-                    1
-                ][1],
-                chebi,
-            )
-            additional_ref_triples = role_between_classes(
-                None, compound_name, METAL_RESISTANCE
-            )
-            additional_comp_triples = role_between_classes(
-                None, compound_name, METAL_RESISTANCE
-            )
+            try:
+                compound_name = term_from_label(
+                    next(filter(lambda t: t[1][0] == args[2], atoms.get("compound_name")))[
+                        1
+                    ][1],
+                    chebi,
+                )
+                additional_ref_triples = role_between_classes(
+                    None, compound_name, METAL_RESISTANCE
+                )
+                additional_comp_triples = role_between_classes(
+                    None, compound_name, METAL_RESISTANCE
+                )
+            except TypeError:
+                "Could not find compound name in atoms."
         case _:
             additional_ref_triples = []
             additional_comp_triples = []
@@ -271,7 +301,7 @@ def write_exhibits_phenotype_to_graph(
         + additional_ref_triples
     )
     ref_phtype = find_or_create_node(
-        hypo_graph, ref_query_trips, id_prefix="P-REF", namespace=phenotype_namespace
+        hypo_ds, h_graph, ref_query_trips, id_prefix="P-REF", namespace=phenotype_namespace
     )
 
     # Fetch the compared phenotype from the graph if it exists,
@@ -283,20 +313,20 @@ def write_exhibits_phenotype_to_graph(
         + additional_comp_triples
     )
     comp_phtype = find_or_create_node(
-        hypo_graph, comp_query_trips, id_prefix="P", namespace=phenotype_namespace
+        hypo_ds, h_graph, comp_query_trips, id_prefix="P", namespace=phenotype_namespace
     )
     return ref_phtype, comp_phtype
 
 
-def hypothesis_to_triples(h):
+def hypothesis_to_triples(h, h_graph):
     triples = []
 
     # Represent the logic program
-    state_1 = logic_program_to_state(h["logic_program"])
+    state_1 = logic_program_to_state(h["logic_program"], h_graph)
 
     # Match the amino acid
     amino_acid = term_from_label(h["observable"], chebi)  # Case-sensitive?
-    print(amino_acid)
+    # print(amino_acid)
     # state_2 =
 
     # Generate `observable` concepts for the reference
@@ -308,3 +338,28 @@ def hypothesis_to_triples(h):
     # State implies State
 
     return triples
+
+
+def add_hypothesis_as_new_graph_in_hypo_ds(hypo_ds, h):
+    """Add a hypothesis as a new graph in the hypothesis dataset."""
+    hid = create_id(prefix="H")
+    h_graphid = rdflib.URIRef(f"http://hypo.project-genesis.io/{hid}")
+    h_graph = hypo_ds.graph(h_graphid)
+    h_graph.bind("hypo", HYPO)
+    h_graph.bind("obo", OBO)
+    h_graph.bind("owl", OWL)
+    h_graph.bind("rdf", RDF)
+    h_graph.bind("rdfs", RDFS)
+
+    triples = hypothesis_to_triples(h, h_graph)
+    for t in triples:
+        h_graph.add(t)
+
+    # Hack for now to remove empty hypothesis graphs
+    # This is not good for future, we want to include
+    # all triples related to the hypothesis
+    if len(h_graph) == 0:
+        print("No triples were added to the graph.")
+        hypo_ds.remove_graph(h_graphid)
+
+    return hypo_ds
--- todo.md
+++ todo.md
@@ -1,3 +1,9 @@
 - [ ] Rewrite Fuseki database script to write to `Dataset` rather than `Graph`.
 - [ ] Implement logic for all phenotypes in Daniel's logic formulae
     - [ ] Resistance to chemicals
+
+## Future
+
+- [ ] If there are overlapping triples between hypotheses, write extra quads to the graph to include the triples in both (or indeed all) contexts. Currently, no new triples are written if they are all in the graph already. This can be problematic for reproducing versions of the dataset with different subsets of hypotheses. We need each hypothesis to be self-contained.
+    - [ ] Will introduce duplication, maybe there is a neater way of doing this. Probably not though, without using something like RDF-star.
+    - [ ] Need good queries for returing dataset versions given a subset of the hypotheses.
